b13396d89fa20b4f33050d186855d99941d6daf2
Sultonkulov99 <abdukhoshim99@gmail.com>
2025-07-07 14:27:37 +0500

    dars4 github action

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private readonly userService: UserService){}
+
+    @Get(":name")
+    getUser(@Param("name") name : string){
+        return this.userService.getUser(name)
+    }  
+    @Get("all")
+    getall(){
+        return this.userService.getAll()
+    }   
+
+    @Post()
+    createUser(@Body() payload : any){
+        return this.userService.createUser(payload)
+    }
+
+    @Get()
+    getAllUsers(){
+        return this.userService.getAllUsers()
+    }
+
+    @Put(":id")
+    updateUser(@Param("id") id: string ,@Body() payload :any){
+        return this.userService.updateUser(id,payload)
+    }
+      
+}
